d8cdb47309e49b8aa22e3b028ccb56c5c87787c1
gabrielrochasouza <gabrielrs7@outlook.com>
2023-08-14 01:30:39 -0300

    refactor

--- src/consts/homePageMessage.js
+++ src/consts/homePageMessage.js
@@ -1,6 +1,6 @@
 export const homePageMessage = `
     <h1>App is running!!</h1>
-    <h3>Rotas</h3>
+    <h3>Endpoints</h3>
     <ul>
         <li>GET <a href='/users'>/users</a></li>
         <li>GET /users/:id</li>

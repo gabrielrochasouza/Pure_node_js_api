2456186cc859989f4dad3c605f10492cfe48835d
gabrielrochasouza <gabrielrs7@outlook.com>
2023-08-14 12:59:59 -0300

    refactor

--- src/middlewares/uniqueFieldRequiredMiddleware.js
+++ src/middlewares/uniqueFieldRequiredMiddleware.js
@@ -1,17 +1,23 @@
 export function uniqueFieldRequiredMiddleware (req, res, entityTable, field) {
+    const pathId = req.params?.id;
 
-    const entityRowWithSameField = entityTable.find(entity => entity[field] === req.body[field])
+    const response = {
+        "statusCode": 400,
+        "message": `Unique ${field} is required`,
+    }
+    let entityRowWithSameField
+
+    if (pathId) {
+        entityRowWithSameField = entityTable.some(entity => entity[field] === req.body[field] && entity.id !== pathId)
+    } else {
+        entityRowWithSameField = entityTable.some(entity => entity[field] === req.body[field])
+    }
 
     if(entityRowWithSameField) {
-        const response = {
-            "statusCode": 400,
-            "message": `Unique ${field} is required`,
-        }
-        
         res.writeHead(400, `Unique ${field} is required`, {'Content-type': 'application/json'})
             .end(JSON.stringify(response))
         return true
     }
-
+    
     return false
 }

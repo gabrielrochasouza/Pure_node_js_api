ff7947db132053a50253e209a4d769fe16e847b2
gabrielrochasouza <gabrielrs7@outlook.com>
2023-08-13 23:26:34 -0300

    refactoring

--- src/utils/buildRoutePath.js
+++ src/utils/buildRoutePath.js
@@ -1,9 +1,6 @@
 export const buildRoutePath = (path) => {
     const regex = /:([a-zA-Z]+)/g
-    let pathWithParams
-    while (path.includes(regex)){
-        pathWithParams = path.replace(regex, '(?<$1>[a-z0-9\-_]+)')
-    }
+    const pathWithParams = path.replace(regex, '(?<$1>[a-z0-9\-_]+)')
 
     const pathRegex = new RegExp(`^${pathWithParams}`)
 

8d9d5a1fda365251dd75119c3ebb20d458aef537
gabrielrochasouza <gabrielrs7@outlook.com>
2023-08-14 01:28:51 -0300

    feat: adding home page message

---
+++ src/consts/homePageMessage.js
@@ -0,0 +1,12 @@
+export const homePageMessage = `
+    <h1>App is running!!</h1>
+    <h3>Rotas</h3>
+    <ul>
+        <li>GET <a href='/users'>/users</a></li>
+        <li>GET /users/:id</li>
+        <li>POST /users</li>
+        <li>POST /users/login</li>
+        <li>PUT /users</li>
+        <li>DELETE /users/:id</li>
+    </ul>
+`
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { buildRoutePath } from './utils/buildRoutePath.js';
 import { usersController } from './controllers/users.controller.js';
+import { homePageMessage } from './consts/homePageMessage.js';
 
 export const routes = [
     {
         method: 'GET',
         path: '/',
         buildRoutePath: buildRoutePath('/'),
-        handler: (req,res) => res.end('App is running 🎉')
+        handler: (req,res) => res.end(homePageMessage)
     },
     ...usersController,
 ]
